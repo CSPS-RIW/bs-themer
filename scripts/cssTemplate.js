78cde7e91c75da47fb327533a5c6b8233188b3d6
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-17 13:14:31 -0400

    create css template

---
+++ scripts/cssTemplate.js
@@ -0,0 +1,77 @@
+let cssTemplate = `
+
+/* Card styles */
+.card-themer .card.card-standard {
+    border-color: ${userColour};
+}
+
+/* Base icon styles */
+.ribbon,
+.ribbon-top,
+.circle {
+	--icon-colour: ${userColour};
+	min-height: 100px;
+	position: relative;
+}
+
+.ribbon::before,
+.ribbon-top::before,
+.circle::before {
+		position: absolute;
+		display: inline-block;
+		background-repeat: no-repeat;
+		text-rendering: auto;
+		-webkit-font-smoothing: antialiased;
+		font-family: 'Font Awesome 5 Free';
+		height: 48px;
+		width: 48px;
+		font-size: 2rem;
+		color: var(--icon-colour);
+		top: -30px;
+		left: -40px;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		padding: 1.75rem;
+		align-items: center;
+		z-index: 2;
+	}
+
+.ribbon::after,
+.ribbon-top::after,
+.circle::after {
+    content: '';
+    position: absolute;
+    height: 60px;
+    width: 60px;
+    top: -25px;
+    left: -35px;
+    background-color: #fff;
+    transform: rotate(45deg);
+    z-index: 1;
+    border: 4px solid var(--icon-colour);
+}
+
+/* icon style */
+.circle::after {
+		border-radius: 50%;
+		width: 70px;
+		height: 70px;
+		top: -30px;
+		left: -40px;
+	}
+
+
+/* icons */
+.dyk::before {
+		content: '\f0eb';
+		font-weight: 900;
+    }
+
+.bug::before {
+		content: '\f188';
+		font-weight: 900;
+    }
+
+
+`;

4ec3f13fe1faeda054c3fcfcd8d5d25813be2a9c
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-17 13:32:46 -0400

    Cleanup file

--- scripts/cssDownload.js
+++ scripts/cssDownload.js
@@ -1,27 +1,5 @@
-// 1. Create a CSS template or use an existing CSS file
-const cssTemplate = `
-  /* Initial CSS styles */
-  .myElement {
-    color: black;
-    background: white;
-  }
-`;
-
-// 2. Capture user input (e.g., changing the background color)
-const userInput = '#ff0000'; // User-selected color
-
-// 3. Modify CSS dynamically
-const dynamicStyles = `
-  .myElement {
-    background: ${userInput};
-  }
-`;
-
-// 4. Create a new CSS file
-const modifiedCSS = cssTemplate + dynamicStyles;
-
-// 5. Download the CSS file
-function downloadCSS(filename, text) {
+// Download CSS file
+export function downloadCSS(filename, text) {
 	const element = document.createElement('a');
 	element.setAttribute(
 		'href',
@@ -36,6 +14,3 @@ function downloadCSS(filename, text) {
 
 	document.body.removeChild(element);
 }
-
-// Usage: Call this function when the user wants to download the CSS file
-downloadCSS('modified.css', modifiedCSS);

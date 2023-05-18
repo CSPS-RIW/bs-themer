76d3361c7b1a1272b4daccf99fa8b7db577dcd62
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-18 09:55:04 -0400

    customize component sample behaviour

--- main.js
+++ main.js
@@ -98,3 +98,14 @@ colorChanger.addEventListener('input', () => {
 	);
 	// ribbonColor.style.setProperty('--icon-colour', colorChanger.value);
 });
+
+// Code sample stuff
+
+let compSample = Array.from(document.querySelectorAll('.component-sample'));
+
+compSample.map((component) => {
+	let copyBtn = component.querySelector('.copy-btn');
+	let componentName = component.querySelector('div').classList[0];
+	copyBtn.setAttribute('title', `Copy code for ${componentName} component`);
+	copyBtn.innerText = `Copy ${componentName} component`;
+});

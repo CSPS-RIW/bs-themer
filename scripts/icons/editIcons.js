9e0ae33cbcac8d18caf871b31ea29f4035d38b9b
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-18 14:23:43 -0400

    Add options manually

--- scripts/icons/editIcons.js
+++ scripts/icons/editIcons.js
@@ -1,31 +1,36 @@
-// fetch iconlist
+// fetch iconlist and create select menu with all icons
 const loadIconList = async () => {
 	let optionsArray = [];
 	try {
 		const resp = await fetch('/iconlist.json');
 		let data = await resp.json();
-		// console.log(data);
-		// Create edit icon button
-		let cards = Array.from(document.querySelectorAll('.card'));
+
+		// fetch data from public dir
 		let { iconList } = data;
-		// console.log(iconList);
 		let iconNames = Array.from(Object.keys(iconList));
 		let iconValues = Object.values(iconList);
-		console.log(iconNames, iconValues);
-		iconNames.map((icon, index) => {
-			let option = document.createElement('opiton');
-			option.setAttribute('value', icon);
-			option.innerText = icon;
-			optionsArray.push(option);
-		});
-		cards.map((card) => {
-			let select = document.createElement('select');
-			select.append(iconValues);
-			!card.classList.contains('copy-this') &&
-				card.insertAdjacentElement('beforebegin', select);
-		});
 
-		console.log(optionsArray);
+		// create an option for each icon name
+
+		for (let i = 0; i < iconNames.length; i++) {
+			let option = document.createElement('option');
+			option.setAttribute('value', iconNames[i]);
+			option.innerText = iconNames[i];
+			optionsArray.push(option);
+			// console.log(option);
+		}
+
+		// Target cards
+		let cards = Array.from(document.querySelectorAll('.card'));
+		// add select and options of each icon to each card
+		cards.map((card, i) => {
+			if (!card.classList.contains('copy-this')) {
+				let select = document.createElement('select');
+				select.append(optionsArray[0]);
+				console.log(optionsArray[0]);
+				card.insertAdjacentElement('beforebegin', select);
+			}
+		});
 	} catch (error) {
 		console.error('Error fetching icons:', error);
 	}

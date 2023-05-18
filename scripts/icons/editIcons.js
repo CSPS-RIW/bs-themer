cc2062f56f0619cc4d6b251aff36619a11284877
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-18 15:18:53 -0400

    Add comments

--- scripts/icons/editIcons.js
+++ scripts/icons/editIcons.js
@@ -14,22 +14,33 @@ const loadIconList = async () => {
 		// Target cards
 		let cards = Array.from(document.querySelectorAll('.card'));
 		// add select and options of each icon to each card
-		cards.map((card) => {
+		cards.map((card, index) => {
 			if (!card.classList.contains('copy-this')) {
+				// create label for a11y
+				let label = document.createElement('label');
+				label.innerText = 'Choose an icon';
+				label.setAttribute('for', `options_${index}`);
+
+				// create select and options
 				let select = document.createElement('select');
+				select.id = `options_${index}`;
 				for (let i = 0; i < iconNames.length; i++) {
 					let option = document.createElement('option');
 					option.setAttribute('value', iconNames[i]);
 					option.innerText = iconNames[i];
+					// Add content for css ::before
 					option.setAttribute(
 						'data-content',
 						iconValues[i][1].content,
 					);
+					// Add font size info for css ::before
 					option.setAttribute(
 						'data-fontWeight',
 						iconValues[i][1].fontWeight,
 					);
-					console.log();
+
+					// add dynamic elements to dom
+					select.insertAdjacentElement('beforebegin', label);
 					select.append(option);
 					card.insertAdjacentElement('beforebegin', select);
 				}

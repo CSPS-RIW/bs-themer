4a2c809b4a2a78bfc71a027bdaf53acbbaa11f71
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-24 15:59:46 -0400

    Find bugs in prod

--- scripts/css-stuff/scss-js.js
+++ scripts/css-stuff/scss-js.js
@@ -1,9 +1,14 @@
 export const loadScss = async () => {
 	try {
 		// ! this doesn't work in prod. Fetch the actual css file
-		let resp = await fetch('/assets/scss/base/icons/_icons.scss');
+		console.log(import.meta.env.PROD);
+		let path = !import.meta.env.PROD
+			? '/assets/scss/base/icons/_icons.scss'
+			: './assets/index-a87d64c4.css';
+		let resp = await fetch(path);
 		let data = await resp.text();
 
+		// ! In prod, data is different so regex doesn't match
 		let pattern = /:root \{([\s\S]*)\}/m;
 		let matches = data.match(pattern);
 		let cssCode = matches ? matches[0] : '';

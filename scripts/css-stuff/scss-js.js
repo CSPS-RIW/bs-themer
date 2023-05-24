525a388046fe261404ae00c866e8442bdef163c9
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-24 15:33:17 -0400

    Add fetch file and clean a bit

--- scripts/css-stuff/scss-js.js
+++ scripts/css-stuff/scss-js.js
@@ -1,4 +1,4 @@
-const loadScss = async () => {
+export const loadScss = async () => {
 	try {
 		let resp = await fetch('/assets/scss/base/icons/_icons.scss');
 		let data = await resp.text();
@@ -7,15 +7,22 @@ const loadScss = async () => {
 		let matches = data.match(pattern);
 		let cssCode = matches ? matches[0] : '';
 		let cssArr = cssCode.split('\\n\\n');
+		// TODO: RM '\n' && ','
 		let cleanCode = '';
+		let cleanStr;
+		// console.log('dirty', cssCode);
 
-		cssArr.map((el) => {
-			cleanCode += el;
-		});
-		console.log(cleanCode);
+		// cssArr.map((el) => {
+		// 	cleanStr = el.replace(/\n/g, '');
+		// 	cleanStr = cleanStr.replace(/\n/g, '');
+		// 	cleanCode += cleanStr;
+		// });
+		// console.log('clean', cleanCode);
+
+		return cssArr;
 	} catch (error) {
 		console.log(`Error fetching scss' ${error}`);
 	}
 };
 
-loadScss();
+// loadScss();

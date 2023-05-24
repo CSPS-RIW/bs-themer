553b2d4d807cfbf92f731fb5a2cbf3eebfd6028e
rtorres <rene.torres@csps-efpc.gc.ca>
2023-05-24 15:37:38 -0400

    use only fetched styles

--- scripts/css-stuff/cssTemplate.js
+++ scripts/css-stuff/cssTemplate.js
@@ -47,86 +47,86 @@ export const updateCssTemplate = async () => {
 	styleTag.textContent = cssIconClasses;
 
 	// Main css template
-	cssTemplate = `:root {
-		--custom-colour: ${userColour}
-	}
-	
-	/* Text Styles  */
-	body .data h1 {
-		color: var(--custom-colour);
-		filter: grayscale(0.2);
-	}
-	
-	body .data h2,
-	body .data h3,
-	body .data h4,
-	body .data h5,
-	body .data h6 {
-		color: var(--custom-colour);
-		filter: grayscale(0.6);
-	}
-	
-	/* Card styles */
-	body .data .card.card-standard {
-		border-color:var(--custom-colour);
-	}
-	
-	/* Base icon styles */
-	.icon-diamond,
-	.icon-square,
-	.icon-circle {
-		--icon-colour:var(--custom-colour);
-		min-height: 100px;
-		position: relative;
-	}
-	
-	.icon-diamond::before,
-	.icon-square::before,
-	.icon-circle::before {
-		position: absolute;
-		display: inline-block;
-		background-repeat: no-repeat;
-		text-rendering: auto;
-		-webkit-font-smoothing: antialiased;
-		font-family: 'Font Awesome 5 Free';
-		height: 48px;
-		width: 48px;
-		font-size: 2rem;
-		color: var(--icon-colour);
-		top: -30px;
-		left: -40px;
-		text-align: center;
-		display: flex;
-		justify-content: center;
-		padding: 1.75rem;
-		align-items: center;
-		z-index: 2;
-	}
-	
-	.icon-diamond::after,
-	.icon-square::after,
-	.icon-circle::after {
-		content: '';
-		position: absolute;
-		height: 60px;
-		width: 60px;
-		top: -25px;
-		left: -35px;
-		background-color: #fff;
-		transform: rotate(45deg);
-		z-index: 1;
-		border: 4px solid var(--icon-colour);
-	}
-	
-	/* icon style */
-	.icon-circle::after {
-		border-radius: 50%;
-		width: 70px;
-		height: 70px;
-		top: -30px;
-		left: -40px;
-	}
-	
+	// cssTemplate = `:root {
+	// 	--custom-colour: ${userColour}
+	// }
+
+	// /* Text Styles  */
+	// body .data h1 {
+	// 	color: var(--custom-colour);
+	// 	filter: grayscale(0.2);
+	// }
+
+	// body .data h2,
+	// body .data h3,
+	// body .data h4,
+	// body .data h5,
+	// body .data h6 {
+	// 	color: var(--custom-colour);
+	// 	filter: grayscale(0.6);
+	// }
+
+	// /* Card styles */
+	// body .data .card.card-standard {
+	// 	border-color:var(--custom-colour);
+	// }
+
+	// /* Base icon styles */
+	// .icon-diamond,
+	// .icon-square,
+	// .icon-circle {
+	// 	--icon-colour:var(--custom-colour);
+	// 	min-height: 100px;
+	// 	position: relative;
+	// }
+
+	// .icon-diamond::before,
+	// .icon-square::before,
+	// .icon-circle::before {
+	// 	position: absolute;
+	// 	display: inline-block;
+	// 	background-repeat: no-repeat;
+	// 	text-rendering: auto;
+	// 	-webkit-font-smoothing: antialiased;
+	// 	font-family: 'Font Awesome 5 Free';
+	// 	height: 48px;
+	// 	width: 48px;
+	// 	font-size: 2rem;
+	// 	color: var(--icon-colour);
+	// 	top: -30px;
+	// 	left: -40px;
+	// 	text-align: center;
+	// 	display: flex;
+	// 	justify-content: center;
+	// 	padding: 1.75rem;
+	// 	align-items: center;
+	// 	z-index: 2;
+	// }
+
+	// .icon-diamond::after,
+	// .icon-square::after,
+	// .icon-circle::after {
+	// 	content: '';
+	// 	position: absolute;
+	// 	height: 60px;
+	// 	width: 60px;
+	// 	top: -25px;
+	// 	left: -35px;
+	// 	background-color: #fff;
+	// 	transform: rotate(45deg);
+	// 	z-index: 1;
+	// 	border: 4px solid var(--icon-colour);
+	// }
+
+	// /* icon style */
+	// .icon-circle::after {
+	// 	border-radius: 50%;
+	// 	width: 70px;
+	// 	height: 70px;
+	// 	top: -30px;
+	// 	left: -40px;
+	// }
+	cssTemplate = `
 	/* icons */
 	${cssClasses}
 

c2ca1fa34415a565fef06a22200352cbc6a085e0
Charles Guenther <charles.guenther@csps-efpc.gc.ca>
2023-05-25 09:03:00 -0400

    include components

--- scripts/cssTemplate.js
+++ scripts/cssTemplate.js
@@ -128,7 +128,28 @@ export const updateCssTemplate = () => {
 	/* icons */
 	${cssIconClasses}
 
-	/* accordions */`;
+	/* accordions */
+	
+	body
+		.data
+		.accordion
+		:not(.card-standard):not(.card-graphic).card
+		.card-title
+		button {
+		border: 1px solid var(--card-main-colour);
+		color: var(--card-main-colour);
+	}
+
+	body
+		.data
+		.accordion
+		:not(.card-standard):not(.card-graphic).card
+		.card-title
+		button[aria-expanded='true'] {
+		background: var(--card-main-colour);
+	}
+	
+	`;
 };
 
 // CSS Download
